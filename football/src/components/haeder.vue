9118e2a50674be3fb81a0955d8ebec7d1f37b2e8
maurizio-marotta <mauriziomarotta@live.it>
2023-05-09 18:48:55 +0200

    header social

--- football/src/components/haeder.vue
+++ football/src/components/haeder.vue
@@ -1,37 +1,36 @@
 <script>
 export default {
-  name : 'header'
-}
+  name: "header",
+};
 </script>
 
 <template>
   <div class="header">
     <div class="container d-flex">
-    <div class="top-bar">
-      <div class="logo-left">
-        <a href="">
-          <img src="../assets/sponsor1.png" alt="sponsor-1">
-        </a>
-        <a href="">
-          <img src="../assets/sponsor2.png" alt="sponsor-2">
-        </a>
+      <div class="top-bar">
+        <a href="" class="logo"><img src="../assets/sponsor1.png" alt=""></a>
+        <a href="" class="logo"><img src="../assets/sponsor2.png" alt=""></a>
+        <a href="" class="logo"><img src="../assets/logo-football.png" alt=""></a>
+        <a href="" class="logo social"><span><i class="fa-brands fa-facebook-f"></i></span></a>
+        <a href="" class="logo social"><i class="fa-brands fa-instagram"></i></a>
+        <a href="" class="logo social"><i class="fa-brands fa-twitter"></i></a>
       </div>
-    </div>
     </div>
   </div>
 </template>
 
 <style lang="scss" scoped>
-
-.header{
-  height: 100vh;
-  background-image: url(../assets/slider1-1.jpg);
-}
-.container{
-  align-items: center;
-  height: 30%;
+.header {
   flex-wrap: wrap;
   background-color: rgba(0, 0, 0, 0.315);
-  max-width: 100%;
+}
+
+.container {
+  align-items: center;
+  height: 30%;
+}
+
+.social{
+  font-size: 30px;
 }
 </style>
