65de4c6d5626d003cedabbf37191430abbfb3b8b
maurizio-marotta <mauriziomarotta@live.it>
2023-05-10 18:43:10 +0200

    header finito

--- football/src/components/haeder.vue
+++ football/src/components/haeder.vue
@@ -6,15 +6,24 @@ export default {
 
 <template>
   <div class="header">
-    <div class="container d-flex">
+    <div class="container ">
       <div class="top-bar">
-        <a href="" class="logo"><img src="../assets/sponsor1.png" alt=""></a>
-        <a href="" class="logo"><img src="../assets/sponsor2.png" alt=""></a>
-        <a href="" class="logo"><img src="../assets/logo-football.png" alt=""></a>
+        <a href="" class="logo sponsor"><img src="../assets/sponsor1.png" alt=""></a>
+        <a href="" class="logo sponsor"><img src="../assets/sponsor2.png" alt=""></a>
+        <a href="" class="logo football"><img src="../assets/logo-football.png" alt=""></a>
         <a href="" class="logo social"><span><i class="fa-brands fa-facebook-f"></i></span></a>
         <a href="" class="logo social"><i class="fa-brands fa-instagram"></i></a>
         <a href="" class="logo social"><i class="fa-brands fa-twitter"></i></a>
       </div>
+    </div>
+    <div class="bottom-bar">
+      <a href="#">HOME</a>
+      <a href="#">FIXTURES & RESULTS</a>
+      <a href="#">LEAGUE TABLE</a>
+      <a href="#">PLAYERS</a>
+      <a href="#">GALLERY</a>
+      <a href="#">BLOG</a>
+      <a href="#">CONTACT</a>
     </div>
   </div>
 </template>
@@ -26,11 +35,53 @@ export default {
 }
 
 .container {
+  justify-content: center;
   align-items: center;
   height: 30%;
 }
 
-.social{
-  font-size: 30px;
+.sponsor{
+  margin-top: 5%;
 }
+
+.social{
+  font-size: 20px;
+  color: white;
+  margin-top: 5%;
+}
+.logo{
+  padding: 15px;
+}
+
+.football{
+  width: 80%;
+}
+
+.bottom-bar{
+  padding: 15px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.bottom-bar a{
+  font-weight: bold;
+  display: block;
+  padding: 10px;
+  font-size: 13px;
+  text-decoration: none;
+  color: white;
+  &::after{
+    content: '';
+    height: 4px;
+    width: 0%;
+    transition: width 0.3s;
+    display: block;
+    background-color: white;
+  }
+  &:hover::after{
+    width: 100%;
+  }
+}
+
 </style>
